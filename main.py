677888e9ed22ed60cecf063cc260ff0f370fefa0
lkz0429 <marciolucascmatos@gmail.com>
2026-07-10 23:26:46 -0300

    1.1.9
    Organização do layout da janela main e adição do sistema de associação de livros por cpf

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from tkinter import ttk
 import json
 
 usuarios = {}
-usuario = None
 Janela = None
 entrada_nome = None
 entrada_livro = None
 entrada_cpf = None
 entrada_pontos = None
 busca_cpf = None
+entrada_livro = None
+emprestimo_cpf = None
+
 class usuario:
 
     def __init__(self):
@@ -51,6 +53,28 @@ class usuario:
          entrada_pontos.delete(0, tkinter.END)
          busca_cpf.delete(0, tkinter.END)
 
+    def limpar_campo_associar():
+         
+         entrada_livro.delete(0, tkinter.END)
+         emprestimo_cpf.delete(0, tkinter.END)
+
+    def emprestar():
+
+        global usuarios
+
+        with open("Dados.JSON", "r") as arquivo:
+             usuarios = json.load(arquivo)
+
+        cpf = emprestimo_cpf.get()
+
+        if cpf in usuarios:
+             usuarios[cpf]["livro"] = str(entrada_livro.get())
+
+             with open("Dados.JSON", "w") as arquivo:
+                json.dump(usuarios, arquivo, ensure_ascii=False, indent=2)
+
+        entrada_livro.delete(0, tkinter.END)
+        emprestimo_cpf.delete(0, tkinter.END)
 
     def somar_pontos():
 
@@ -68,9 +92,7 @@ class usuario:
                   json.dump(usuarios, arquivo, ensure_ascii=False, indent=2)
 
         entrada_pontos.delete(0, tkinter.END)
-        busca_cpf.delete(0, tkinter.END)
-        
-                 
+        busca_cpf.delete(0, tkinter.END)               
 
     def adicionar_usuario():
 
@@ -116,6 +138,8 @@ class usuario:
 
     def associar_livro():
 
+        global entrada_livro, emprestimo_cpf
+
         janela = tkinter.Toplevel(Janela)
         janela.title("Empréstimo")
         janela.transient(Janela)
@@ -133,14 +157,23 @@ class usuario:
         topleveldeemprestimo.pack(padx=20, pady=20, side="top")
         topleveldeemprestimo.pack_propagate(False)
 
-        tkinter.Label(topleveldeemprestimo, text="Livro", font=("Arial", 12, "bold")).pack(padx=0, side="top")
+        titulo = tkinter.Label(topleveldeemprestimo, text="Digite o livro a ser associado ao cpf", font=("Arial", 12, "bold"))
+        titulo.pack(padx=0, pady=10, side="top")
+
+        tkinter.Label(topleveldeemprestimo, text="Cpf:", font=("Arial", 12, "bold")).pack(padx=0, side="top")
+        emprestimo_cpf = ttk.Entry(topleveldeemprestimo)
+        emprestimo_cpf.pack(padx=0, pady=10, side="top")
+
+        tkinter.Label(topleveldeemprestimo, text="Livro:", font=("Arial", 12, "bold")).pack(padx=0, side="top")
         entrada_livro = ttk.Entry(topleveldeemprestimo)
         entrada_livro.pack(padx=0, pady=10, side="top")
 
-        tkinter.Label(topleveldeemprestimo, text="Cpf", font=("Arial", 12, "bold")).pack(padx=0, side="top")
-        usuario = ttk.Entry(topleveldeemprestimo)
-        usuario.pack(padx=0, pady=10, side="top")
+        enviar = tkinter.Button(topleveldeemprestimo, text="Associar", command=usuario.emprestar, font=("Arial", 12, "bold"), width=7, height=3)
+        enviar.pack(padx=75, side="right")
           
+        limpar = tkinter.Button(topleveldeemprestimo, text="Limpar", command=usuario.limpar_campo_associar, font=("Arial", 12, "bold"), width=7, height=3)
+        limpar.pack(padx=75, side="left")
+
     def penalizar_por_cpf():
 
         global entrada_pontos, busca_cpf
@@ -165,11 +198,11 @@ class usuario:
         titulo = tkinter.Label(topleveldepenalidades, text="Digite o cpf a ser penalizado", font=("Arial", 12, "bold"))
         titulo.pack(padx=0, pady=10, side="top")
 
-        tkinter.Label(topleveldepenalidades, text="cpf:", font=("Arial", 12, "bold")).pack(padx=0, pady=10, side="top")
+        tkinter.Label(topleveldepenalidades, text="cpf:", font=("Arial", 12, "bold")).pack(padx=0, side="top")
         busca_cpf = ttk.Entry(topleveldepenalidades)
         busca_cpf.pack(padx=0, pady=10, side="top")
 
-        tkinter.Label(topleveldepenalidades, text="quantia:", font=("Arial", 12, "bold")).pack(padx=0, pady=10, side="top")
+        tkinter.Label(topleveldepenalidades, text="quantia:", font=("Arial", 12, "bold")).pack(padx=0, side="top")
         entrada_pontos = ttk.Entry(topleveldepenalidades)
         entrada_pontos.pack(padx=0, pady=10, side="top")
 
@@ -186,7 +219,7 @@ def main():
     Janela.title("Janela principal")
     Janela.geometry("1024x720")
     Janela.minsize(960, 720)
-    Janela.configure(bg="gray")
+    Janela.configure(bg="gray", bd=5, relief="solid")
 
     Head = tkinter.Frame(
 
@@ -209,17 +242,17 @@ def main():
 
     Titulo = ttk.Label(
         Head,
-        text="Seja bem vindo!!\n organizador virtual literário",
-        font=("Arial", 20, "bold"),
+        text="organizador virtual bibliotecário",
+        font=("Arial", 25, "bold"),
         wraplength=400,
     )
-    Titulo.pack()
+    Titulo.pack(pady=5)
 
     Frame_direito = tkinter.Frame(
 
         Janela, 
-        width=300,
-        height=400,
+        width=350,
+        height=300,
         bd=5,
         relief="solid"
     )
@@ -231,39 +264,16 @@ def main():
 
     )
 
-    Descricao = tkinter.Label(Frame_direito, text="Este organizador serve para que sistemas de bibliotecas sejam admnistrados de maneira dinâmica e aumenta a produtividade, serve como um registro de prazos e histórico de cada pessoa a pegar livros emprestados!!!", font=("Arial", 19, "bold"), wraplength=300)
+    Descricao = tkinter.Label(Frame_direito, text="Este organizador serve para que sistemas de bibliotecas sejam admnistrados de maneira dinâmica, serve como um registro de prazos e histórico de cada pessoa a pegar livros emprestados!!!", font=("Arial", 17, "bold"), wraplength=300)
 
-    Descricao.pack(padx=0, pady=0)
+    Descricao.pack(padx=5, pady=20)
 
     Frame_direito.pack_propagate(False)
-
-    Frame_mid = tkinter.Frame(
-    
-            Janela, 
-            width=300,
-            height=250,
-            bd=5,
-            relief="solid"
-        )
-    Frame_mid.pack(
-    
-            padx=40,
-            pady=0,
-            side="right"
-    
-        )
-    
-    Descricao2 = tkinter.Label(Frame_mid, text="O sistema de pontos é uma maneira de penalizar aqueles que descumprem o prazo, você pode verificar a situação dos usuários em cadastros", font=("Arial", 19, "bold"), wraplength=300)
-    
-    Descricao2.pack(padx=0, pady=0)
-    
-    Frame_mid.pack_propagate(False)
-
 
     area_botao = tkinter.Frame(
 
         Janela, 
-        width=200, 
+        width=600, 
         height=300,
         bd=5,
         relief="solid"
@@ -286,7 +296,7 @@ def main():
         width=14,
         height=2,
     )
-    botao1.pack(anchor="center")
+    botao1.pack(anchor="w")
 
     botao2 = tkinter.Button(
         area_botao,
@@ -296,7 +306,7 @@ def main():
         width=14,
         height=2,
     )
-    botao2.pack(anchor="center")
+    botao2.pack(anchor="w")
 
     botao3 = tkinter.Button(
          area_botao,
@@ -306,7 +316,7 @@ def main():
          width=14,
          height=2
     )
-    botao3.pack(anchor="center")
+    botao3.pack(anchor="w")
 
     botao4 = tkinter.Button(
          area_botao,
@@ -316,7 +326,7 @@ def main():
          width=14,
          height=2
     )
-    botao4.pack(anchor="center")
+    botao4.pack(anchor="w")
 
     Janela.mainloop()
 
@@ -327,8 +337,8 @@ def janela_pontos():
     janela = tkinter.Toplevel(Janela)
     janela.title("Cadastros")
     janela.transient(Janela)
-    janela.minsize(768, 824)
-    janela.maxsize(768, 824)
+    janela.minsize(824, 268)
+    janela.maxsize(824, 268)
 
     cadastros = tkinter.Frame(
          janela,
@@ -372,7 +382,6 @@ def janela_pontos():
         )
     )
     tabela.pack(fill="both", expand=True)
-
 
 if __name__ == "__main__":
     main()
